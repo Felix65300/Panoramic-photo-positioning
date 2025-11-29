fbbb2b916bb82018a16560cf37cdd89ed2491be4
JACK <wjulie0425@gmail.com>
2025-11-29 21:39:18 +0800

    新增dataset的資料處理檔和測試檔，以及修改過後的csv檔

---
+++ test.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import pandas as pd
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from data import MyDataset
+import time
+
+csv_data = pd.read_csv("_gkcN1hzqm1RFcsvpk5Xmg/stitched_pano_final.csv")
+
+transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+])
+
+dataset = MyDataset(csv_data, "_gkcN1hzqm1RFcsvpk5Xmg", transform)
+loader = DataLoader(dataset, batch_size=4, shuffle=False)
+
+for i in range(len(dataset)):
+    img, id_label, filename = dataset[i]  # 取得第 i 筆資料
+    print("Image tensor shape:", img.shape)
+    print("Label:", id_label)
+    print("Filename:", filename)
+    time.sleep(0.2)
+
+
+'''
+for i in range(len(dataset)):
+    img, id_label, filename = dataset[i]  # 取得第 i 筆資料
+    print("Image tensor shape:", img.shape)
+    print("Label:", id_label)
+    print("Filename:", filename)
+'''
