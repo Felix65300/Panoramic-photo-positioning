8f31e94fc3efad77f920f07f9b524236c13f0baf
JACK <wjulie0425@gmail.com>
2025-12-11 07:09:09 +0800

    修改檔案路徑位置設置

--- src/test.py
+++ src/test.py
@@ -2,17 +2,21 @@
 import pandas as pd
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from src.data import MyDataset
+from data import MyDataset
 import time
 
-csv_data = pd.read_csv("../_gkcN1hzqm1RFcsvpk5Xmg/stitched_pano_final.csv")
+
+
+#../改成./
+csv_data = pd.read_csv("./_gkcN1hzqm1RFcsvpk5Xmg/stitched_pano_final.csv")
 
 transform = transforms.Compose([
     transforms.Resize((64, 64)),
     transforms.ToTensor()
 ])
 
-dataset = MyDataset(csv_data, "../_gkcN1hzqm1RFcsvpk5Xmg", transform)
+#../改成./
+dataset = MyDataset(csv_data, "./_gkcN1hzqm1RFcsvpk5Xmg", transform)
 loader = DataLoader(dataset, batch_size=4, shuffle=False)
 
 for i in range(len(dataset)):
