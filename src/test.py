6862c42724213845da1aaa8b686199730b948f06
JACK <wjulie0425@gmail.com>
2025-12-17 13:15:30 +0800

    把test.py修改成和main函式一樣

--- src/test.py
+++ src/test.py
@@ -2,21 +2,17 @@
 import pandas as pd
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from data import MyDataset
+from src.data import MyDataset
 import time
 
-
-
-#../改成./
-csv_data = pd.read_csv("./_gkcN1hzqm1RFcsvpk5Xmg/stitched_pano_final.csv")
+csv_data = pd.read_csv("../_gkcN1hzqm1RFcsvpk5Xmg/stitched_pano_final.csv")
 
 transform = transforms.Compose([
     transforms.Resize((64, 64)),
     transforms.ToTensor()
 ])
 
-#../改成./
-dataset = MyDataset(csv_data, "./_gkcN1hzqm1RFcsvpk5Xmg", transform)
+dataset = MyDataset(csv_data, "../_gkcN1hzqm1RFcsvpk5Xmg", transform)
 loader = DataLoader(dataset, batch_size=4, shuffle=False)
 
 for i in range(len(dataset)):
