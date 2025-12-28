fdc32ea8d28101ab07c1627a0b13d2a2bb7dc045
Felix65300 <weihan.hsu@outlook.com>
2025-12-28 16:19:18 +0800

    Figure rewrite (#17)
    * 大改版：CSV編號重設、修改 MyDataSet、手刻 Pano_CNN、完成 Training.py
    
    * 手刻版 CNN Loss 值降到0.01
    
    * 資料集大改，改用IageFolder，1000張圖片獨立存放在編號000~999的子資料夾中，相對應的Training與Inference也進行修改
    
    * 重新訓練，Push新版本的Inference
    
    * 改資料夾名字
    
    * 修改手刻模型Inference_1000
    
    * 成功寫完手刻模型的正確率
    
    * 生出Accuracy的圖
    
    * 刪Loss_curve.py，Rename accuracy.py to figure_accuracy_plus_loss.py
    
    * 完成resnet18_Step1_training_and_testing.py
    
    * 完成Step1_training_and_testing.py
    
    * figure_accuracy_plus_loss.py
    
    * 完成figure_accuracy_plus_loss.py、刪除錯誤的 Accuracy 曲線圖
    
    ---------
    
    Co-authored-by: Felix96530 <D1310197@o365.fcu.edu.tw>

---
+++ src/Custom_model/Step1_training_and_testing.py
@@ -0,0 +1,115 @@
+import os
+import sys
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+# 路徑設定
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+Project_Root = os.path.dirname(parent_dir)
+
+sys.path.append(parent_dir)
+sys.path.append(Project_Root)
+
+from src.data import get_dataset
+from Convolution_Class import CNN
+
+IMG_WIDTH = 512
+IMG_HEIGHT = 128
+BATCH_SIZE = 16
+DEVIVE = torch.device("cuda")
+IMG_PATH = os.path.join(Project_Root, "Dataset_Step1")
+epochs = 200
+
+MODEL_PATH = os.path.join(current_dir, 'pano_cnn_model.pth')
+
+
+def model_training_and_testing():
+    dataset = get_dataset(root_dir=IMG_PATH, width=IMG_WIDTH, height=IMG_HEIGHT, is_train=True)
+
+    trainloader = DataLoader(
+        dataset=dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True
+    )
+
+    model = CNN()
+    model = model.to(DEVIVE)
+    criterion = nn.CrossEntropyLoss().to(DEVIVE)
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode='min', factor=0.5, patience=10, min_lr=1e-6
+    )
+
+    start_epoch = 0
+    best_acc = 0.0
+
+    # if os.path.exists(MODEL_PATH):
+    #     try:
+    #         print(f"Loading weights from {MODEL_PATH}")
+    #         checkpoint = torch.load(MODEL_PATH, map_location=device)
+    #         model.load_state_dict(checkpoint)
+    #         print("Weight loaded successfully.")
+    #     except Exception as e:
+    #         print(f"Loading failed: {e}, training from scratch.")
+    # else:
+    #     print("No existing weights found. Training from scratch.")
+
+    epoch_losses = []
+    epoch_accs = []
+
+    print("Start training...")
+
+    for epoch in range(start_epoch, epochs):
+        model.train()
+        running_loss = 0.0
+        correct = 0
+        total = 0
+
+        with tqdm(trainloader, desc=f"Epoch {epoch + 1}/{epochs}", ncols=100, leave=False) as loop:
+            for img, is_label in loop:
+                img, id_label = img.to(DEVIVE), id_label.to(DEVIVE)
+
+                optimizer.zero_grad()
+
+                outputs = model(img)
+                loss = criterion(outputs, id_label)
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=2.0)
+                optimizer.step()
+
+                running_loss += loss.item()
+                _, predicted = torch.max(outputs.data, 1)
+                total += id_label.size(0)
+                correct += (predicted == id_label).sum().item()
+
+                current_acc = 100 * correct / total
+                loop.set_postfix(loss=f"{loss.item():.4f}", acc=f"{current_acc:.2f}%")
+
+
+        epoch_acc = 100 * correct / total
+        avg_loss = running_loss / len(trainloader)
+        epoch_losses.append(avg_loss)
+        epoch_accs.append(epoch_acc)
+        current_lr = scheduler.get_lr()[0]
+
+        scheduler.step(avg_loss)
+        print(f"Epoch {epoch + 1} Result: Loss={avg_loss:.4f} | Acc={epoch_acc:.2f}% | LR={current_lr:.6f}")
+
+        # 存取最佳權重 ---
+        # if epoch_acc > best_acc:
+        #     best_acc = epoch_acc
+        #     torch.save(model.state_dict(), MODEL_PATH)
+        #     print(f"★ New Best Model Saved! (Acc: {best_acc:.2f}%) saved to {MODEL_PATH}")
+    return epoch_losses, epoch_accs
+
+
+def main():
+    _, _ = model_training_and_testing()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/figure_accuracy_plus_loss.py
@@ -0,0 +1,101 @@
+import matplotlib.pyplot as plt
+from pathlib import Path
+import numpy as np
+import os
+import sys
+
+src = os.path.dirname(os.path.abspath(__file__))
+Project_Root = os.path.dirname(src)
+model_dir = os.path.join(src, 'Custom_model')
+fig_dir = os.path.join(Project_Root, 'Figures')
+resnet18_dir = os.path.join(src, "Resnet18")
+sys.path.append(model_dir)
+sys.path.append(resnet18_dir)
+from Custom_model.Step1_training_and_testing import model_training_and_testing
+from Resnet18.resnet18_Step1_training_and_testing import resnet18_training_and_testing
+
+# --- 1. 全局設置：讓圖片更符合論文要求 ---
+plt.rcParams.update({
+    'font.family': 'serif',         # 使用襯線字體
+    'font.size': 12,                # 全局字體大小
+    'axes.labelsize': 14,           # 軸標籤字體大小
+    'axes.titlesize': 16,           # 子圖標題字體大小
+    'xtick.labelsize': 12,          # X軸刻度字體大小
+    'ytick.labelsize': 12,          # Y軸刻度字體大小
+    'legend.fontsize': 12,          # 圖例字體大小
+    'legend.frameon': True,         # 圖例加上邊框
+    'legend.framealpha': 0.9,       # 圖例邊框透明度
+    'lines.linewidth': 2.5,         # 線條寬度
+    'axes.linewidth': 1.5,          # 座標軸線寬度
+    'grid.linestyle': '--',         # 網格線樣式
+    'grid.alpha': 0.6               # 網格線透明度
+})
+
+color_model = '#1f77b4' # 藍
+color_resnet18 = '#d62728' # 紅
+
+# --- 2. 準備數據 ---
+epochs = np.arange(1, 201)
+
+def main():
+    model_losses, model_accs = model_training_and_testing()
+    resnet18_losses,resnet18_accs = resnet18_training_and_testing()
+
+    # ===========================
+    # 2. 設定論文繪圖風格
+    # ===========================
+    # 使用類似 seaborn 的白底網格風格作為基礎
+    plt.style.use("seaborn-v0_8-whitegrid")
+
+    # ===========================
+    # 3. 創建畫布和子圖
+    # ===========================
+    # 創建一個圖形框架，包含 1 列 2 行的子圖
+    fig, (ax1, ax2) = plt.subplots(nrows=1, nols=2, figsize=(14,6))
+
+    # ===========================
+    # 4. 繪製左側子圖：Loss Curve 比較
+    # ===========================
+    # 繪製手刻模型的 Loss 曲線
+    ax1.plot(epochs, model_losses, label="Model", color=color_model, linestyle='-')
+    # 繪製 Resnet 18 的 Loss 曲線
+    ax1.plot(epochs, resnet18_losses, label="Resnet18", color=color_resnet18, linestyle='--')
+
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss Value")
+    ax1.set_title("Training Loss Comparison")
+    ax1.legend(loc='upper right') # 圖例位置
+
+    # ============================================
+    # 5. 繪製右側子圖：Accuracy Curve 比較
+    # ============================================
+    # 繪製 Model A 的 Accuracy 曲線
+    ax2.plot(epochs, model_accs, label="Model", color=color_model, linestyle='-')
+    # 繪製 Model B 的 Accuracy 曲線
+    ax2.plot(epochs, resnet18_accs, label="Resnet18", color=color_resnet18, linestyle='--')
+
+    ax2.set_xlabel("Epochs")
+    ax2.set_ylabel("Accuracy (%)")
+    ax2.set_title("Accuracy Comparison")
+    ax2.legend(loc='lower right')
+    ax2.set_ylim(0, 105)
+
+    # ===========================
+    # 6. 最後調整與存檔
+    # ===========================
+    # 自動調整子圖間距，避免標籤重疊
+    plt.tight_layout()
+
+    # 儲存圖片
+    # dpi=300 是印刷品質的標準
+    # bbox_inches='tight' 確保儲存時去除多餘白邊
+    save_path = os.path.join(fig_dir, 'Loss_curve_and_Accuracy.png')
+    plt.savefig(save_path, dpi=300, bbox_inches='tight')
+
+    print(f"圖片已儲存至: {save_path}")
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
