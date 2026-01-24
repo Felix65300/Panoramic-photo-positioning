27e77342af01a94f7191821afa88aa117c3a3408
Felix96530 <D1310197@o365.fcu.edu.tw>
2026-01-24 16:12:00 +0800

    完成 __init__.py

--- src/DA/__init__.py
+++ src/DA/__init__.py
@@ -2,4 +2,12 @@
 from .grid_mask import GridMask
 from .color_temperature import RandomColorTemperature
 from .brightness import RandomBrightness
-from .builder import get_transform
+from .builder import get_transforms
+
+# 定義當有人使用 from DA import * 時會拿到什麼
+__all__ = [
+    'GridMask',
+    'RandomColorTemperature',
+    'RandomBrightness',
+    'get_transforms'
+]
