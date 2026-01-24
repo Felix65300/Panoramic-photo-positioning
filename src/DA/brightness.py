bd15b35fac91f7017bcfbaf3f6f0ccca64ced23e
Felix96530 <D1310197@o365.fcu.edu.tw>
2026-01-24 15:59:10 +0800

    完成 brightness.py

---
+++ src/DA/brightness.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+from torchvision import transforms
+
+class RandomBrightness(nn.Module):
+    """
+    隨機調整料度與對比度
+    封裝 torchvision.transforms.ColorJitter 以保持介面一致
+    """
+    def __init__(self, brightness = 0.2, contrast = 0.2):
+        super().__init__()
+        # brightness = 0.2 代表亮度在 [0.8, 1.2] 之間隨機變化
+        # brightness 設定的是正負的範圍 (例如：正負0.2)
+        self.jitter = transforms.ColorJitter(brightness=brightness, coutrast=contrast)
+
+    def forward(self, img):
+        return self.jitter(img)
