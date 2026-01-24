e023f55adfbb5be417200a437860d2c7111b35f4
Felix96530 <D1310197@o365.fcu.edu.tw>
2026-01-24 17:07:08 +0800

    更新 __init__.py

--- src/DA/__init__.py
+++ src/DA/__init__.py
@@ -2,6 +2,7 @@
 from .grid_mask import GridMask
 from .color_temperature import RandomColorTemperature
 from .brightness import RandomBrightness
+from .horizontal_roll import RandomHorizontalRoll
 from .builder import get_transforms
 
 # 定義當有人使用 from DA import * 時會拿到什麼
@@ -9,5 +10,6 @@ __all__ = [
     'GridMask',
     'RandomColorTemperature',
     'RandomBrightness',
+    'RandomHorizontalRoll',
     'get_transforms'
 ]
