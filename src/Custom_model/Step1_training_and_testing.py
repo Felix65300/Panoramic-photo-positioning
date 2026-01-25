526222331e298b5fdab518074098fe0a437c70b1
Felix65300 <weihan.hsu@outlook.com>
2026-01-25 20:46:44 +0800

    Da (#21)
    * 大改版：CSV編號重設、修改 MyDataSet、手刻 Pano_CNN、完成 Training.py
    
    * 手刻版 CNN Loss 值降到0.01
    
    * 資料集大改，改用IageFolder，1000張圖片獨立存放在編號000~999的子資料夾中，相對應的Training與Inference也進行修改
    
    * 重新訓練，Push新版本的Inference
    
    * 改資料夾名字
    
    * 修改手刻模型Inference_1000
    
    * 成功寫完手刻模型的正確率
    
    * 生出Accuracy的圖
    
    * 完成 grid_mask.py
    
    * 完成 color_temperature.py
    
    * 完成 brightness.py
    
    * 完成 builder.py
    
    * 完成 brightness.py
    
    * 完成 __init__.py
    
    * 解除載權重的註解
    
    * 訂正錯字
    
    * 將 data.py 中的左右隨機平一獨立出來，寫成 horizontal_roll.py
    
    * 更新 builder.py
    
    * 更新 __init__.py
    
    * 更新 .gitignore，新增data，必且更名為 data_Step.py
    
    * 完成 data_Step2.py，並且更新所有 import data_Step1 改成 data_Step2.py
    
    * 訂正 brightness.py 的錯字
    
    * 訂正 builder.py 的錯字
    
    * 修改 color_temperature.py 的縮排
    
    * 訂正 grid_mask.py 的錯字
    
    * 測試成功
    
    ---------
    
    Co-authored-by: Felix96530 <D1310197@o365.fcu.edu.tw>

--- src/Custom_model/Step1_training_and_testing.py
+++ src/Custom_model/Step1_training_and_testing.py
@@ -14,13 +14,13 @@ Project_Root = os.path.dirname(parent_dir)
 sys.path.append(parent_dir)
 sys.path.append(Project_Root)
 
-from src.data import get_dataset
+from src.data_Step2 import get_dataset
 from Convolution_Class import CNN
 
 IMG_WIDTH = 512
 IMG_HEIGHT = 128
 BATCH_SIZE = 16
-DEVIVE = torch.device("cuda")
+DEVICE = torch.device("cuda")
 IMG_PATH = os.path.join(Project_Root, "Dataset_Step1")
 epochs = 200
 
@@ -37,8 +37,8 @@ def model_training_and_testing():
     )
 
     model = CNN()
-    model = model.to(DEVIVE)
-    criterion = nn.CrossEntropyLoss().to(DEVIVE)
+    model = model.to(DEVICE)
+    criterion = nn.CrossEntropyLoss().to(DEVICE)
     optimizer = optim.Adam(model.parameters(), lr=1e-4)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, mode='min', factor=0.5, patience=10, min_lr=1e-6
@@ -47,16 +47,16 @@ def model_training_and_testing():
     start_epoch = 0
     best_acc = 0.0
 
-    # if os.path.exists(MODEL_PATH):
-    #     try:
-    #         print(f"Loading weights from {MODEL_PATH}")
-    #         checkpoint = torch.load(MODEL_PATH, map_location=device)
-    #         model.load_state_dict(checkpoint)
-    #         print("Weight loaded successfully.")
-    #     except Exception as e:
-    #         print(f"Loading failed: {e}, training from scratch.")
-    # else:
-    #     print("No existing weights found. Training from scratch.")
+    if os.path.exists(MODEL_PATH):
+        try:
+            print(f"Loading weights from {MODEL_PATH}")
+            checkpoint = torch.load(MODEL_PATH, map_location=DEVICE)
+            model.load_state_dict(checkpoint)
+            print("Weight loaded successfully.")
+        except Exception as e:
+            print(f"Loading failed: {e}, training from scratch.")
+    else:
+        print("No existing weights found. Training from scratch.")
 
     epoch_losses = []
     epoch_accs = []
@@ -71,7 +71,7 @@ def model_training_and_testing():
 
         with tqdm(trainloader, desc=f"Epoch {epoch + 1}/{epochs}", ncols=100, leave=False) as loop:
             for img, id_label in loop:
-                img, id_label = img.to(DEVIVE), id_label.to(DEVIVE)
+                img, id_label = img.to(DEVICE), id_label.to(DEVICE)
 
                 optimizer.zero_grad()
 
@@ -94,16 +94,16 @@ def model_training_and_testing():
         avg_loss = running_loss / len(trainloader)
         epoch_losses.append(avg_loss)
         epoch_accs.append(epoch_acc)
-        current_lr = scheduler.get_last_lr()[0]
+        current_lr = optimizer.param_groups[0]['lr']
 
         scheduler.step(avg_loss)
         print(f"Epoch {epoch + 1} Result: Loss={avg_loss:.4f} | Acc={epoch_acc:.2f}% | LR={current_lr:.6f}")
 
         # 存取最佳權重 ---
-        # if epoch_acc > best_acc:
-        #     best_acc = epoch_acc
-        #     torch.save(model.state_dict(), MODEL_PATH)
-        #     print(f"★ New Best Model Saved! (Acc: {best_acc:.2f}%) saved to {MODEL_PATH}")
+        if epoch_acc > best_acc:
+            best_acc = epoch_acc
+            torch.save(model.state_dict(), MODEL_PATH)
+            print(f"★ New Best Model Saved! (Acc: {best_acc:.2f}%) saved to {MODEL_PATH}")
     return epoch_losses, epoch_accs
 
 
