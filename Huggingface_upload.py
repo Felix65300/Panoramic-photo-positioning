2a6b256e44cc2624ac0d9ae94f5cac0ec0b694b3
D1310197 <D1310197@o365.fcu.edu.tw>
2026-04-07 23:25:18 +0800

    新增上傳資料集到 Hugging Face 的程式碼，這樣以後要載資料集時只需要呼叫 API 就好了

---
+++ Huggingface_upload.py
@@ -0,0 +1,22 @@
+import os
+from datasets import load_dataset
+
+# 本地根目錄 (包含 Brightness, Mask 等子資料夾)
+DATASET_PATH = "Panoramic-photo-positioning/Dataset_Step2"
+# 命名規範：帳號/專案名稱
+REPO_NAME = "Felix96430/NSTC-Panoramic-photo-positioning"
+def upload_dataset():
+    print(f"🚀 開始掃描階層架構：{DATASET_PATH}...")
+
+    dataset = load_dataset("imgagefolder",data_dir=DATASET_PATH)
+
+    print(dataset)
+
+    print(f"☁️ 準備推送到 Hugging Face Hub ({REPO_NAME})...")
+    # private=True 設定為私人，防止資料集外傳
+    dataset.upload(REPO_NAME,private=True)
+
+    print("✅ 上傳完成！")
+
+if __name__ == "__main__":
+    upload_dataset()
