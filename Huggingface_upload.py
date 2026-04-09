19eb0058e7134a6c3504c228620b022076f0a195
D1310197 <D1310197@o365.fcu.edu.tw>
2026-04-09 21:28:29 +0800

    重新登入 Huggingface Access token & 刪除 Datasets 與 Dataset_Step2 的 .cache 資料夾

--- Huggingface_upload.py
+++ Huggingface_upload.py
@@ -1,19 +1,23 @@
-import os
-
 from huggingface_hub import HfApi
 
-# 🚀 1. 初始化 API
-api = HfApi()
-repo_name = "Felix96430/Panoramic-photo-positioning"
+def upload_to_huggingface():
+    # 🚀 1. 初始化 API
+    api = HfApi()
+    repo_name = "Panoramic-photo-positioning/Panoramic-photo-positioning-Step2"
 
-print("啟動 API 穩健上傳模式")
+    print(f"啟動 API 穩健上傳模式，REPO NAME: {repo_name}")
 
-# 🚀 3. 執行資料夾同步 (支援斷點續傳)
-api.upload_large_folder(
-    folder_path="Datasets/Dataset_Step2",
-    repo_id=repo_name,
-    repo_type="dataset"
-)
+    api.create_repo(repo_id=repo_name, repo_type="dataset", exist_ok=True)
+
+    # 🚀 2. 執行資料夾同步 (支援斷點續傳)
+    api.upload_large_folder(
+        folder_path="Datasets/Dataset_Step2",
+        repo_id=repo_name,
+        repo_type="dataset"
+    )
 
 
-print("✅ 所有圖片上傳完畢！")
+    print("✅ 所有圖片上傳完畢！")
+
+if __name__ == "__main__":
+    upload_to_huggingface()
