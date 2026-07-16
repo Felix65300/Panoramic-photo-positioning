7958dfb30e2e98dc5bef2fa74de4524b0f8cad0e
王浩閔 <wjulie0425@gmail.com>
2026-07-16 23:10:29 +0800

    Mobilenet (#48)
    * 新增data_Step2_5.py
    
    * 修改moblienet的全連接層
    
    * 測試模型後確定能練
    
    * 完成
    
    * 優化寫法，以及加入新增excel
    
    * 修改excel寫法
    
    * 修改路徑
    
    * 修改存資料夾的路徑位置
    
    * 修改畫圖方式
    
    * 生完圖了
    
    * 修改程式防止兩次壓縮，跟刪掉預訓練檔，然後重train

--- src/Mobilenet_V3_small/Mobilenet_V3_small_modified_version.py
+++ src/Mobilenet_V3_small/Mobilenet_V3_small_modified_version.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 
 def build_model(num_classes):
-    model = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.DEFAULT)
+    model = models.mobilenet_v3_small(weights=None)
     in_features = model.classifier[3].in_features
     model.classifier[3] = nn.Linear(in_features, num_classes)
 
