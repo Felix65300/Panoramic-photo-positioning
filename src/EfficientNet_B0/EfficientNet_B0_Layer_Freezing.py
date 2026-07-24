58abbcaa912255b05d7331730f67e7073f1cb8bd
Felix65300 <weihan.hsu@outlook.com>
2026-07-24 22:07:54 +0800

    Efficient net layer freezing (#53)
    * 新增上傳資料集到 Hugging Face 的程式碼，這樣以後要載資料集時只需要呼叫 API 就好了
    
    * 刪除冗餘程式碼
    
    * 修改錯誤 API，將 dataset.upload() 改成 dataset.push_to_hub()
    
    * 重新排列資料集的層級
    
    * 重新登入 Huggingface Access token & 刪除 Datasets 與 Dataset_Step2 的 .cache 資料夾
    
    * 檢查 Huggingface 是否成功上傳 21000０片張圖片
    
    * 修 push 的 bug
    
    * 重新設定圖片大小為 512 * 128 並重新上傳
    
    * 修改 grid_mask.py 程式碼偏移量並修改遮罩起始位置與加一個 batch 變數讓 method 可以運行。修改 DA_ataset_Frame.py 程式碼，將加遮罩的圖片 罩的圖片改儲存成 png 檔
    
    * 重新生成遮罩圖片並上傳
    
    * 修改color_temperature的code (#31)
    
    * 建 Step2.5 的檔案、刪除 Huggingface
    
    * 改訓練檔
    
    * 區分 data_Step2_5
    
    * 區分 data_Step2_5，測試時可以不用重複 resize
    
    * 建 Step2.5 的檔案、刪除 Huggingface
    
    * 改訓練檔
    
    * 區分 data_Step2_5
    
    * 區分 data_Step2_5，測試時可以不用重複 resize
    
    * 修改 .gitignore
    
    * 修改 Data_Step2_5，拆成Data_Step2_5_Test 跟 Data_Step2_5_Train，並修改Step2.5_TraingAndTesting
    
    * 在Step2.5_TrainingAndTesting.py 的 DataLoader 加入 num_workers, pin_memory 參數，
    
    * 修改 Data_Step_2_5_Test.py
    
    * 修改 Step2.5_TrainningAndTesting.py
    
    * 刪除Figures/Step2.5所有資料夾
    
    * 重新生成 Figures/Step2.確名稱的子資料夾
    
    * 重新設計水平位移幅度的邏輯，保留最大精度
    
    * 刪除所有 DataLoader 中出現的 num_workers, pin_memory 參數
    
    * 訓練完成
    
    * 完成 GeM
    
    * 完成 GeM 調整與測試
    
    * 完成 Layer Freezing 模型定義
    
    * 完成 Layer Freezing 測試
    
    ---------
    
    Co-authored-by: D1310197 <D1310197@o365.fcu.edu.tw>
    Co-authored-by: 王浩閔 <wjulie0425@gmail.com>

---
+++ src/EfficientNet_B0/EfficientNet_B0_Layer_Freezing.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+# ==================================
+# 變因二：權重凍結 (Layer Freezing)
+# ==================================
+
+def build_model(num_classes):
+    # 1. 載入 ImageNet 預訓練的 EfficientNet-B0
+    weights = models.EfficientNet_B0_Weights.DEFAULT
+    model = models.efficientnet_b0(weights=weights)
+
+    # 2. 重建分類頭
+    model.classifier = nn.Sequential(
+        nn.Dropout(p=0.2, inplace=True),
+        nn.Linear(1280, num_classes)
+    )
+
+    # 3. 實作凍結策略
+    # 設定凍結範圍：凍結 Stem 層與前 3 個 MBConv 區塊 (索引 0, 1, 2, 3)
+    freeze_until_index = 4
+
+    for idx, child in enumerate(model.features):
+        if idx < freeze_until_index:
+            for param in child.parameters():
+                param.requires_grad = False
+
+    return model
+
+def __main__():
+    model = build_model(1000)
+    print(model)
+
+if __name__ == '__main__':
+    __main__()
